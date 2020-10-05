90cd28c7e2298026d55f4e830be9c4dec29703a8
NCSaketh <nc.saketh1@gmail.com>
2020-10-05 23:36:58 +0530

    UC1 : Valid First Name

--- src/com/cg/userregistration/UserRegistration.java
+++ src/com/cg/userregistration/UserRegistration.java
@@ -1,5 +1,24 @@
 package com.cg.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	public static void main(String[] args) {
+		ValidFirstName();
+
+	}
+
+	public static boolean ValidFirstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name");
+		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", sc.next())) {
+			System.out.println("Not a valid First Name , Please repeat.");
+			return ValidFirstName();
+		} else {
+			System.out.println("Valid First Name");
+			return true;
+		}
+	}
 
 }
