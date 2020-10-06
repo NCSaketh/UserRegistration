cb89442928172d61c3e654f8f47fdf08d979377f
NCSaketh <nc.saketh1@gmail.com>
2020-10-06 20:18:40 +0530

    UC9 : Checking sample emails

--- src/com/cg/userregistration/UserRegistration.java
+++ src/com/cg/userregistration/UserRegistration.java
@@ -16,7 +16,8 @@ public class UserRegistration {
 	public static boolean ValidFirstName() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name");
-		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", sc.next())) {
+		String string = sc.nextLine();
+		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", string)) {
 			System.out.println("Not a valid First Name! Please enter again.");
 			return ValidFirstName();
 		} else {
@@ -28,7 +29,8 @@ public class UserRegistration {
 	public static boolean ValidLastName() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Last Name");
-		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", sc.next())) {
+		String string = sc.nextLine();
+		if (!Pattern.matches("[A-Z]{1}[a-z]{2,}", string)) {
 			System.out.println("Not a valid Last Name! Please enter again.");
 			return ValidLastName();
 		} else {
@@ -40,8 +42,10 @@ public class UserRegistration {
 	public static boolean validEmailID() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your email");
-		if (Pattern.matches("[\\w]{3,}([.][a-zA-Z]{3,})*[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,}([.][a-zA-Z]{2,})*",
-				sc.next())) {
+		String string = sc.nextLine();
+		if (Pattern.matches(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$",
+				string)) {
 			System.out.println("Valid Email");
 			return true;
 		} else {
@@ -53,8 +57,8 @@ public class UserRegistration {
 	public static boolean validMobileNo() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your mobile number along with country code");
-		String s = sc.nextLine();
-		if (Pattern.matches("[1-9]{1}[0-9]{1}[\\s]{1}[1-9]{1}[0-9]{9}", s)) {
+		String string = sc.nextLine();
+		if (Pattern.matches("[1-9]{1}[0-9]{1}[\\s]{1}[1-9]{1}[0-9]{9}", string)) {
 			System.out.println("Valid mobile phone");
 			return true;
 		} else {
@@ -66,8 +70,9 @@ public class UserRegistration {
 	public static boolean validPassword() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your password");
-		if (Pattern.matches("(.*[A-Z]+.*)", sc.next()) && Pattern.matches("[\\S]{8,}", sc.next())
-				&& Pattern.matches("(.*[0-9]+.*)", sc.next()) && Pattern.matches("(.*[\\W]{1}.*)",sc.next())) {
+		String string = sc.nextLine();
+		if (Pattern.matches("(.*[A-Z]+.*)", string) && Pattern.matches("[\\S]{8,}", string)
+				&& Pattern.matches("(.*[0-9]+.*)", string) && Pattern.matches("(.*[\\W]{1}.*)", string)) {
 			System.out.println("Valid password");
 			return true;
 		} else {
